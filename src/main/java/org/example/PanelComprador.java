cddc8492f12205bf5437dcfa9b7b409683dc2bb3
MartinL <martin.lara.tsx@gmail.com>
2023-11-08 20:58:50 -0300

    feat: Event System Setup

--- src/main/java/org/example/PanelComprador.java
+++ src/main/java/org/example/PanelComprador.java
@@ -2,6 +2,8 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PanelComprador extends JPanel {
     private Comprador c = new Comprador();
@@ -15,13 +17,60 @@ public class PanelComprador extends JPanel {
 
         rightPanel.add(new JLabel(" Saldo: $" + c.getSaldo())); // Aqui podemos agregar las opciones de monedas
         rightPanel.add(midrightPanel);
-        midrightPanel.add(new JButton("COCACOLA"));
-        midrightPanel.add(new JButton("FANTA"));
-        midrightPanel.add(new JButton("SPRITE"));
-        midrightPanel.add(new JButton("SUPER8"));
-        midrightPanel.add(new JButton("SNICKERS"));
-        midrightPanel.add(new JButton("VUELTO"));
+
+
+        JButton coca = new JButton("COCACOLA");
+        JButton fan = new JButton("FANTA");
+        JButton spr = new JButton("SPRITE");
+        JButton s8 = new JButton("SUPER8");
+        JButton sn = new JButton("SNICKERS");
+        JButton vuel = new JButton("VUELTO");
+
+        midrightPanel.add(coca);
+        midrightPanel.add(fan);
+        midrightPanel.add(spr);
+        midrightPanel.add(s8);
+        midrightPanel.add(sn);
+        midrightPanel.add(vuel);
         add(rightPanel, BorderLayout.EAST);
+
+        coca.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("test");
+            }
+        });
+
+        fan.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("test2");
+            }
+        });
+        spr.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("test3");
+            }
+        });
+        s8.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("test4");
+            }
+        });
+        sn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("test5");
+            }
+        });
+        vuel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("test6");
+            }
+        });
     }
     @Override
     public void paint(Graphics g) {
