57df68a0e3e85bc986399bfcf846ce5452621901
Benjamin <benjamin.villarroel3@gmail.com>
2023-11-08 15:15:07 -0400

    feat: improved GUI

--- src/main/java/org/example/PanelExpendedor.java
+++ src/main/java/org/example/PanelExpendedor.java
@@ -14,5 +14,16 @@ public class PanelExpendedor extends JPanel {
         super.paint(g);
         g.setColor(Color.BLUE);
         g.fillRect(300,0,387,600);
+        g.setColor(Color.LIGHT_GRAY);
+        // Rectángulos grises que representan los depósitos de cada producto
+        g.fillRect(320,20,350,60);
+        g.fillRect(320,110,350,60);
+        g.fillRect(320,200,350,60);
+        g.fillRect(320,290,350,60);
+        g.fillRect(320,380,350,60);
+        // Depósitos gráficos de monedas y producto
+        g.setColor(Color.BLACK);
+        g.fillRect(340,465,120,80);
+        g.fillRect(520,465,120,80);
     }
 }
