735049e5ee2965d36cdb01616cc89cdc3e6fe0d2
Benjamin <benjamin.villarroel3@gmail.com>
2023-11-08 21:01:25 -0300

    feat: added ActionListener in PanelComprador. Now buttons can do actions

--- src/main/java/org/example/PanelComprador.java
+++ src/main/java/org/example/PanelComprador.java
@@ -2,11 +2,21 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class PanelComprador extends JPanel {
+public class PanelComprador extends JPanel implements ActionListener {
     private Comprador c = new Comprador();
+    private JButton coca, fanta, sprite, super8, snickers, agregarMoneda;
     public PanelComprador() {
         setLayout(new BorderLayout());
+
+        coca = new JButton("CocaCola");
+        fanta = new JButton("Fanta");
+        sprite = new JButton("Sprite");
+        super8 = new JButton("Super 8");
+        snickers = new JButton("Snickers");
+        agregarMoneda = new JButton("Agregar monedas");
 
         JPanel rightPanel = new JPanel();
         JPanel midrightPanel = new JPanel();
@@ -15,12 +25,18 @@ public class PanelComprador extends JPanel {
 
         rightPanel.add(new JLabel(" Saldo: $" + c.getSaldo())); // Aqui podemos agregar las opciones de monedas
         rightPanel.add(midrightPanel);
-        midrightPanel.add(new JButton("COCACOLA"));
-        midrightPanel.add(new JButton("FANTA"));
-        midrightPanel.add(new JButton("SPRITE"));
-        midrightPanel.add(new JButton("SUPER8"));
-        midrightPanel.add(new JButton("SNICKERS"));
-        midrightPanel.add(new JButton("VUELTO"));
+        midrightPanel.add(coca);
+        coca.addActionListener(this);
+        midrightPanel.add(super8);
+        super8.addActionListener(this);
+        midrightPanel.add(fanta);
+        fanta.addActionListener(this);
+        midrightPanel.add(snickers);
+        snickers.addActionListener(this);
+        midrightPanel.add(sprite);
+        sprite.addActionListener(this);
+        midrightPanel.add(agregarMoneda);
+        agregarMoneda.addActionListener(this);
         add(rightPanel, BorderLayout.EAST);
     }
     @Override
@@ -28,5 +44,25 @@ public class PanelComprador extends JPanel {
         super.paint(g);
         g.setColor(Color.BLACK);
         g.fillOval(500,200, 40, 40);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Con este esqueleto podemos comenzar a trabajar en qué acción efectuará cada botón. Rellené con casos ejemplo solamente. Conectar lógica del resto del código
+        if(e.getSource() == coca) {
+            System.out.println("Compraste una Coca");
+        }
+        if(e.getSource() == fanta) {
+            System.out.println("Compraste una Fanta");
+        }
+        if(e.getSource() == sprite) {
+            System.out.println("Compraste una Sprite");
+        }
+        if(e.getSource() == super8) {
+            System.out.println("Compraste un Super 8");
+        }
+        if(e.getSource() == snickers) {
+            System.out.println("Compraste un Snickers");
+        }
     }
 }
